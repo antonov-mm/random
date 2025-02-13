3c625d2b6880b7a9e7e6cf2146173d470b976d14
Максим <antonov.maxim.98@icloud.com>
2025-02-13 14:07:48 +0400

    Обновление /components/theme-toggle.tsx

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -8,7 +8,7 @@ export function ThemeToggle() {
   const { theme, setTheme } = useTheme()
 
   return (
-    <Button variant="ghost" size="icon" onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
+    <Button className="ml-0 mr-auto" variant="ghost" size="icon" onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
       <Sun className="h-6 w-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <Moon className="absolute h-6 w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
